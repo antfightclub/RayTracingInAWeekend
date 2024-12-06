eb91bd4fc2195ec803095a2a63946cc839aa49ed
monapink <91607167+antfightclub@users.noreply.github.com>
2024-12-06 09:17:23 +0100

    Sec5-5 fixed

--- RayTracingTheNextWeek/perlin.h
+++ RayTracingTheNextWeek/perlin.h
@@ -68,7 +68,7 @@ private:
 
 		for (int i = 0; i < 2; i++) 
 			for (int j = 0; j < 2; j++)
-				for (int k = 0; i < 2; k++) {
+				for (int k = 0; k < 2; k++) {
 					vec3 weight_v(u - i, v - j, w - k);
 					accum += (i * uu + (1 - i) * (1 - uu))
 						   * (j * vv + (1 - j) * (1 - vv))
