b0626c10cd048ebdc682c276f274b55bf1c5028f
monapink <91607167+antfightclub@users.noreply.github.com>
2024-12-08 13:02:51 +0100

    Sec2-2 Running estimate of pi

--- RayTracingTheRestOfYourLife/pi.cc
+++ RayTracingTheRestOfYourLife/pi.cc
@@ -7,15 +7,17 @@ int main() {
 	std::cout << std::fixed << std::setprecision(12);
 	
 	int inside_circle = 0;
-	int N = 100000;
+	int runs = 0;
 
-	for (int i = 0; i < N; i++) {
+	while (true) {
+		runs++;
 		auto x = random_double(-1, 1);
 		auto y = random_double(-1, 1);
 		if (x * x + y * y < 1) {
 			inside_circle++;
 		}
-	}
 
-	std::cout << "Estimate of Pi = " << (4.0 * inside_circle) / N << '\n';
+		if (runs % 100000 == 0)
+			std::cout << "\rEstimate of Pi = " << (4.0 * inside_circle) / runs;
+	}
 }
