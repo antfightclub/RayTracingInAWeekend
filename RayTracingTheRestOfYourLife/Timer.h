9c15668b3c488dc8189cd37f659b4c4e17e25a5b
monapink <91607167+antfightclub@users.noreply.github.com>
2024-12-08 12:50:02 +0100

    Copy state from last book

---
+++ RayTracingTheRestOfYourLife/Timer.h
@@ -0,0 +1,24 @@
+#ifndef MY_TIMER_H
+#define MY_TIMER_H
+
+#include <chrono>
+#include <numeric>
+
+class Timer {
+private: 
+	using Clock = std::chrono::steady_clock;
+	using Second = std::chrono::duration<double, std::ratio<1> >;
+
+	std::chrono::time_point<Clock> m_beg{ Clock::now() };
+
+public: 
+	void reset() {
+		m_beg = Clock::now();
+	}
+
+	double elapsed() const {
+		return std::chrono::duration_cast<Second>(Clock::now() - m_beg).count();
+	}
+};
+
+#endif
